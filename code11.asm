1d557b593a33e017814b35fc29b6b6bcf961ae97
im19850618 <im19850618@gmail.com>
2026-05-26 17:49:00 -0700

    Add assembly code for multiplication of two numbers

---
+++ code11.asm
@@ -0,0 +1,18 @@
+section .data
+    num1 dq 184467440715445113
+    num2 dq 1544511315445113
+    
+section .text
+    global _start
+_start:
+    mov rax, [num1]
+    mov rbx, [num2]
+    mul rbx
+
+    ; 184467440715445113×1544511315445113 = 284888554042335236103385884419369
+    ; Lower 64 bits (RAX)=284888554042335236103385884419369 mod 2^64 = 17277107872285376345
+    ; Upper 64 bits (RDX)=284888554042335236103385884419369 ÷   2^64 = 15445110589888
+    
+    mov rax,60
+    xor rdi,rdi ;zf=1
+    syscall
